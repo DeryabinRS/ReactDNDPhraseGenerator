0578a65bd7b1a67ab8c6fc7994d1f6cca493b198
DeryabinRS <deryabinrs@gmail.com>
2021-08-26 17:34:57 +0700

    create app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {ReactFragment as RF} from 'react';
+import { useState } from 'react';
+import styled from 'styled-components'
+
+const CardStyle = styled.div`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  cursor:grab;
+`
 
 function App() {
+  const [cardList, setCardList] = useState([
+    {id: 1, order: 3, text: 'Card 1'},
+    {id: 2, order: 1, text: 'Card 2'},
+    {id: 3, order: 4, text: 'Card 3'},
+    {id: 4, order: 2, text: 'Card 4'},
+  ]);
+
+  function dragStartHandler(e: React.MouseEvent, card: any){
+    console.log(card)
+  }
+  function dragLeaveHandler(e: React.MouseEvent){
+
+  }
+  function dragOverHandler(e: React.MouseEvent){
+
+  }
+  function dragEndHandler(e: React.MouseEvent){
+
+  }
+  function dragDropHandler(e: React.MouseEvent, card: any){
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {cardList.map((card,i) => 
+        <div key={i}
+          onDragStart={(e) => dragStartHandler(e, card)}  
+          onDragLeave={(e) => dragLeaveHandler(e)}
+          onDragOver={(e) => dragOverHandler(e)}
+          onDragEnd={(e) => dragEndHandler(e)}
+          onDrop={(e) => dragDropHandler(e, card)}
+
+          className={'card'} 
+          draggable={true}>
+          {card.text}
+        </div>
+      )}
     </div>
   );
 }
