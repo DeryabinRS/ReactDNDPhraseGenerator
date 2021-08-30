787673a4a04f9a42c2bdf43e02ccd9b7e391c39f
DeryabinRS <deryabinrs@gmail.com>
2021-08-30 17:21:58 +0700

    add cards items

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,145 @@
 import React, { useState } from "react"
 import { DragDropContext, Draggable, Droppable, DropResult } from "react-beautiful-dnd"
+import styled from 'styled-components'
 import "./App.css"
 
+const currentString = "I don't know what the facking GraphQL"
 
-const listItems = [
-	{id: "1",name: "Study Spanish"},
-	{id: "2",name: "Workout"},
-	{id: "3",name: "Film Youtube"},
-	{id: "4",name: "Grocery Shop"}
-]
+const listItems = (str:string) => str.split(' ').map((item, index) => {
+  return {id: String(index + 1), name: item, col: 1}
+}).sort(() => Math.random() - 0.5)
 
+const grid = 8;
 
 const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
-	padding: 10,
-	display: `inline-block`,
-	margin: `0 50px 15px 50px`,
-	background: isDragging ? "#4a2975" : "white",
-	color: isDragging ? "white" : "black",
-	border: `1px solid black`,
-	fontSize: `20px`,
-	borderRadius: `5px`,
-	
-	...draggableStyle
-})
+  padding: grid * 2,
+  margin: `0 ${grid}px 0 0`,
+  trnsition: '.3s',
+  background: isDragging ? 'lightgreen' : 'lightgrey',
+  ...draggableStyle,
+});
 
+const getItemCard = styled.div<{isDragging: boolean}>`
+	padding: ${grid} * 2;
+	margin: 0 ${grid}px 0 0;
+	trnsition: .3s;
+	background: ${props => (props.isDragging ? 'lightgreen' : 'lightgrey')};
+`;
+
+const WordList = styled.div`
+	display: flex;
+	justify-content: center;
+	padding: grid;
+	overflow: auto;
+	height:50px;
+	background: lightblue;
+`;
+
+const PhraseBox = styled.div`
+	display: flex;
+	justify-content: center;
+	padding: grid;
+	overflow: auto;
+	background: lightgray;
+	height:50px;
+	margin-top:50px;
+`;
+interface ICard {
+	id: string;
+	name:string;
+	col: number
+}
 function App() {
-	const [ todo, setTodo ] = useState(listItems)
+
+	//const [initialData, setInitialData] = useState(listItems(currentString))
+
+	const [ cards, setCards ] = useState<ICard[]>(listItems(currentString))
 
 	const onDragEnd = (result: DropResult) => {
-		const { source, destination } = result
+		const { source, destination, draggableId } = result
 		if (!destination) return
 
-		const items = Array.from(todo)
-		const [ newOrder ] = items.splice(source.index, 1)
-		items.splice(destination.index, 0, newOrder)
+		if(
+			destination.droppableId === source.droppableId &&
+			destination.index === source.index
+		){return}
 
-		setTodo(items)
+		const start = source.droppableId;
+		const finish = destination.droppableId;
+		
+		const items = Array.from(cards)
+		const [ newOrder ] = items.splice(source.index, 1)
+
+		if(start === finish){
+			items.splice(destination.index, 0, newOrder)
+			setCards(items)
+			console.log(result)
+		}else{
+			const newCol = +finish.slice(-1);
+			newOrder.col = newCol
+			items.splice(destination.index, 0, newOrder)
+			console.log(newOrder, newCol)
+		}
 	}
+
 	return (
 		<div className="App">
-			<h1>Drag and Drop</h1>
+			<h1>Collect the phrase</h1>
 			<DragDropContext onDragEnd={onDragEnd}>
-				<Droppable droppableId="todo" direction="horizontal">
-					{(provided) => (
-						<div className="todo" {...provided.droppableProps} ref={provided.innerRef}>
-							{todo.map(({ id, name }, index) => {
-								return (
-									<Draggable key={id} draggableId={id} index={index}>
-										{(provided, snapshot) => (
-											<div
-												ref={provided.innerRef}
-												{...provided.draggableProps}
-												{...provided.dragHandleProps}
-												style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
-											>
-												{name}
-											</div>
-										)}
-									</Draggable>
-								)
+				<Droppable droppableId="col1" direction="horizontal">
+					{(provided, snapshot) => (
+						<WordList {...provided.droppableProps} ref={provided.innerRef}>
+							{cards.map(({ id, name, col }, index) => {
+								if(col === 1){
+									return (
+										<Draggable key={id} draggableId={id} index={index}>
+											{(provided, snapshot) => (
+												<div
+													ref={provided.innerRef}
+													{...provided.draggableProps}
+													{...provided.dragHandleProps}
+													style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
+												>
+													{name}
+												</div>
+											)}
+										</Draggable>
+									)
+								}
 							})}
-						</div>
+							{provided.placeholder}
+						</WordList>
 					)}
 				</Droppable>
+				
+				<Droppable droppableId="col2" direction="horizontal">
+					{(provided, snapshot) => (
+						<PhraseBox {...provided.droppableProps} ref={provided.innerRef}>
+							{cards.map(({ id, name, col }, index) => {
+								if(col === 2){
+									return (
+										<Draggable key={id} draggableId={id} index={index}>
+											{(provided, snapshot) => (
+												<div
+													ref={provided.innerRef}
+													{...provided.draggableProps}
+													{...provided.dragHandleProps}
+													style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
+												>
+													{name}
+												</div>
+											)}
+										</Draggable>
+									)
+								}
+							})}
+							{provided.placeholder}
+						</PhraseBox>
+					)}
+				</Droppable>
+
 			</DragDropContext>
+			<b>Drag a word to this field</b>
 		</div>
 	)
 }
